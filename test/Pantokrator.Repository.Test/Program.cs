21536727ad30c336c900f49e8e81a94fbabadfa0
Emre Karahan <emre.karahan@gmail.com>
2018-04-23 02:05:28 +0300

    changes

---
+++ test/Pantokrator.Repository.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pantokrator.Repository.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
